8eb46e68eaab454750b0bdef07eb21bd9259ea75
aanchal <aanchalverma721@gmail.com>
2024-12-08 12:19:39 +0530

    changes done

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Login from "./pages/Login"
+import { ThemeProvider } from "@mui/material"
+import { theme } from "./utils/MuiComponents"
+import './App.css'
+import { Dashboard } from "./pages/Dashboard"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+
+  return (
+    <>
+       <BrowserRouter>
+    <ThemeProvider theme={theme}>
+    <ToastContainer />
+      <Routes>
+        {/* <Route path="/dashboard" element={<Dashboard/>} /> */}
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </ThemeProvider>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
